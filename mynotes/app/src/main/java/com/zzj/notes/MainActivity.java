1258e78cad00033fb110e0c107bf8548e59659d5
yuanjinlong <295987351@qq.com>
2017-04-05 16:50:57 +0800

    添加注册登录

--- mynotes/app/src/main/java/com/zzj/notes/MainActivity.java
+++ mynotes/app/src/main/java/com/zzj/notes/MainActivity.java
@@ -1,103 +1,128 @@
 package com.zzj.notes;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
-import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener {
+import com.zzj.notes.utils.PreferenceHelper;
+import com.zzj.notes.utils.SystemUtils;
+import com.zzj.notes.widget.base.NoteBaseActivity;
+import com.zzj.notes.widget.view.LookNoteActivity;
 
-	private Button textbtn, imgbtn, videobtn;
-	private ListView lv;
-	private Intent i;
-	private MyAdapter adapter;
-	private NotesDB notesDB;
-	private SQLiteDatabase dbReader;
-	private Cursor cursor;
+public class MainActivity extends NoteBaseActivity implements View.OnClickListener {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		initView();
-	}
+    private TextInputLayout inputPassLayout;
+    private TextInputLayout inputValiaPassLayout;
+    private TextInputEditText userPass;
+    private TextInputEditText valiaUserPass;
+    private Button login;
 
-	public void initView() {
-		lv = (ListView) findViewById(R.id.list);
-		textbtn = (Button) findViewById(R.id.text);
-		imgbtn = (Button) findViewById(R.id.img);
-		videobtn = (Button) findViewById(R.id.video);
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_main;
+    }
 
-		textbtn.setOnClickListener(this);
-		imgbtn.setOnClickListener(this);
-		videobtn.setOnClickListener(this);
+    public void initView() {
+        inputPassLayout = (TextInputLayout) findViewById(R.id.id_input_pass);
+        inputValiaPassLayout = (TextInputLayout) findViewById(R.id.id_valia_view);
+        userPass = (TextInputEditText) findViewById(R.id.user_password);
+        valiaUserPass = (TextInputEditText) findViewById(R.id.input_valia_password);
+        login = (Button) findViewById(R.id.login);
+        inputValiaPassLayout.setError("输入密码不正确");
+        inputPassLayout.setError("输入密码不正确");
+        if (TextUtils.isEmpty(readPass())) {
+            inputValiaPassLayout.setVisibility(View.VISIBLE);
+            login.setText("注册");
+        } else {
+            inputValiaPassLayout.setVisibility(View.GONE);
+            login.setText("登录");
+        }
+        userPass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
 
-		notesDB = new NotesDB(this);
-		dbReader = notesDB.getReadableDatabase();
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                inputPassLayout.setErrorEnabled(false);
+            }
 
-		lv.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void afterTextChanged(Editable editable) {
 
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				cursor.moveToPosition(position);
-				Intent i = new Intent(MainActivity.this, SelectAct.class);
-				i.putExtra(NotesDB.ID,
-						cursor.getInt(cursor.getColumnIndex(NotesDB.ID)));
-				i.putExtra(NotesDB.CONTENT, cursor.getString(cursor
-						.getColumnIndex(NotesDB.CONTENT)));
-				i.putExtra(NotesDB.TIME,
-						cursor.getString(cursor.getColumnIndex(NotesDB.TIME)));
-				i.putExtra(NotesDB.PATH,
-						cursor.getString(cursor.getColumnIndex(NotesDB.PATH)));
-				i.putExtra(NotesDB.VIDEO,
-						cursor.getString(cursor.getColumnIndex(NotesDB.VIDEO)));
-				startActivity(i);
-			}
-		});
+            }
+        });
 
-	}
-	/** 点击添加数据 */
-	@Override
-	public void onClick(View v) {
-		i = new Intent(this, AddContent.class);
-		switch (v.getId()) {
-		case R.id.text:
-			i.putExtra("flag", "1");
-			startActivity(i);
-			break;
+        valiaUserPass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-		case R.id.img:
-			i.putExtra("flag", "2");
-			startActivity(i);
-			break;
+            }
 
-		case R.id.video:
-			i.putExtra("flag", "3");
-			startActivity(i);
-			break;
-		}
-	}
-	/** listview的数据源 */
-	public void selectDB() {
-		cursor = dbReader.query(NotesDB.TABLE_NAME, null, null, null, null,
-				null, null);
-		adapter = new MyAdapter(this, cursor);
-		lv.setAdapter(adapter);
-	}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                inputValiaPassLayout.setErrorEnabled(false);
+            }
 
-	/** 在点击保存数据 回到主界面后调用 */
-	@Override
-	protected void onResume() {
-		super.onResume();
-		selectDB();
-	}
+            @Override
+            public void afterTextChanged(Editable editable) {
 
+            }
+        });
+        login.setOnClickListener(this);
+    }
+
+    private void savePass(String pass) {
+        if (!TextUtils.isEmpty(pass)) {
+            PreferenceHelper.write(this, "note_data", "note_pass", pass);
+        }
+    }
+
+    private String readPass() {
+        return PreferenceHelper.readString(this, "note_data", "note_pass");
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login:
+                loginAndRegister();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void loginAndRegister() {
+        String userPassStr = userPass.getText().toString();
+        if (TextUtils.isEmpty(userPassStr)) {
+            userPass.setError("请输入密码");
+            inputPassLayout.setErrorEnabled(true);
+        }
+        if (TextUtils.isEmpty(readPass())) {
+            String valiaPassStr = valiaUserPass.getText().toString();
+            if (!userPassStr.equals(valiaPassStr)) {
+                inputValiaPassLayout.setErrorEnabled(true);
+                valiaUserPass.setError("输入密码不一致，请重新输入");
+            } else if (userPassStr.equals(valiaPassStr)) {
+                savePass(userPassStr);
+            }
+        } else {
+            String pass = readPass();
+            if (pass.equals(userPassStr)) {
+                Toast.makeText(this, "登录成功", Toast.LENGTH_SHORT).show();
+                SystemUtils.jumpActivity(this, LookNoteActivity.class);
+            } else {
+                valiaUserPass.setError("密码错误");
+                inputPassLayout.setEnabled(true);
+                Toast.makeText(this, "输入密码错误", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
 }
